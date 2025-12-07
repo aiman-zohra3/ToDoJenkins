88848c936a95950355ce42e0928b0e5abf09f249
aimanzohra02 <aimanzohra02@gmail.com>
2025-12-07 17:37:54 +0500

    Optimize pipeline for low memory environment

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js'],
   testTimeout: 60000,
+  // Ignore nested example project to avoid haste map collisions
+  testPathIgnorePatterns: ["/simple-todo-app-mongodb-express-node-master/"],
   verbose: true
 };
 
